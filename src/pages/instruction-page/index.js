222fb259807c9526444daf622a6143008fa3e715
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-05-08 23:01:46 -0300

    Adicionar o texto da destruicao

--- src/pages/instruction-page/index.js
+++ src/pages/instruction-page/index.js
@@ -4,7 +4,15 @@ import {PageContainer} from './styled'
 export default function InstructionPage() {
  return (
    <PageContainer>
-    InstructionPage
+    <div>
+      <p>
+        Burrrrp!! Eu e Morty decidimos visitar a Cidadela Rick em uma dimensão paralela. Era governada por diversos Ricks e Mortys de diferentes realidades, sabia disso?
+
+        Mas chegando lá, tudo já tava uma bagunça! Tinha eleição pra presidente Morty rolando e uma agitação política que deixou o clima bem pesado. E aí, como se não bastasse, uma onda de energia misteriosa atingiu a Cidadela! Foi coisa de louco! Edifícios desmoronaram, destroços voaram e todo mundo entrou em pânico e a população foi dizimada.
+
+       
+      </p>
+    </div>
    </PageContainer>
  );
 }
