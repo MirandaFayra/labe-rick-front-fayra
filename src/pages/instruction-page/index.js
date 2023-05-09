d698625ac9e74af51c708936e27154048a7687cc
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-05-08 23:22:48 -0300

    Finalizar a pagina de instrucoes

--- src/pages/instruction-page/index.js
+++ src/pages/instruction-page/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import {PageContainer} from './styled'
+import {PageContainer,TextContainer} from './styled'
 
 export default function InstructionPage() {
  return (
    <PageContainer>
-    <div>
+    <TextContainer>
       <p>
-        Burrrrp!! Eu e Morty decidimos visitar a Cidadela Rick em uma dimensão paralela. Era governada por diversos Ricks e Mortys de diferentes realidades, sabia disso?
+        Burrrrp!! Eu e Morty decidimos visitar a Cidadela Rick em uma dimensão paralela. Era governada por diversos Ricks e Mortys de diferentes realidades, sabia disso?<br/>
 
-        Mas chegando lá, tudo já tava uma bagunça! Tinha eleição pra presidente Morty rolando e uma agitação política que deixou o clima bem pesado. E aí, como se não bastasse, uma onda de energia misteriosa atingiu a Cidadela! Foi coisa de louco! Edifícios desmoronaram, destroços voaram e todo mundo entrou em pânico e a população foi dizimada.
+        <br/>Mas chegando lá, tudo já tava uma bagunça! Tinha eleição pra presidente Morty rolando e uma agitação política que deixou o clima bem pesado. E aí, como se não bastasse, uma onda de energia misteriosa atingiu a Cidadela! Foi coisa de louco! Edifícios desmoronaram, destroços voaram e todo mundo entrou em pânico e a população foi dizimada. <br/>
+
+        <br/> <span>Com isso, precisamos da sua ajuda para popular novamente o planeta! </span> <br/>
 
        
       </p>
-    </div>
+    </TextContainer>
    </PageContainer>
  );
 }
--- src/pages/instruction-page/styled.js
+++ src/pages/instruction-page/styled.js
@@ -1,12 +1,16 @@
 import styled from 'styled-components';
 import space from '../../assets/space-background.png'
+import theme from '../../constants/THEME';
 
 export const PageContainer = styled.div`
     height: 86vh;
     width: 100vw;
+    display: flex;
     background: url(${space});
     background-repeat: no-repeat;
     background-size: 100vw;
+    align-items: center;
+    justify-content: center;
 
 
     @media (max-width: 1015px) {
@@ -14,6 +18,21 @@ export const PageContainer = styled.div`
         background-repeat: no-repeat;
         background-size: auto;
     }
-    
+
+`
+
+export const TextContainer = styled.div`
+    width: 40vw;
+    height: 60vh;
+    background:${theme.colors.blue100};
+    align-items: center;
+    justify-content: center;
+    padding: 2rem;
+    border-radius:5px;
+    opacity: 0.8;
+
+    span{
+        font-weight:bolder;
+    }
 `
 
