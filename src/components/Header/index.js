3423248065c8cd17d1e33de2a539c0f366b1367b
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-05-08 22:07:16 -0300

    Styles: Finalizar a estilizacao do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,15 @@
 import * as S from './styled'
 import Arrow  from '../../assets/leftArrow.svg'
+import Rocket from '../../assets/rocket.svg'
 
 export default function Header(){
     return(
+        <S.HeaderContainer>
         <S.HeaderStyle>
             <img src={Arrow} alt='flexa para esquerda'/>
             <p>Labe Rick</p>
-            <img alt='icone de casa'/>
+            <img src={Rocket} alt='icone de casa'/>
         </S.HeaderStyle>
+        </S.HeaderContainer>
     )
 }
