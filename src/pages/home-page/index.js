e837ecec0e93ef24b0785b9d804e802974411d6a
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-05-09 13:50:01 -0300

    Structure: Fazer a base  da página home

--- src/pages/home-page/index.js
+++ src/pages/home-page/index.js
@@ -5,7 +5,13 @@ export default function HomePage() {
  return (
   <PageContainer>
     <TextContainer>
-   
+      <h2>Boas Vindas, NOME</h2>
+        <p>Bem Vinde ao planeta Rick! <br/>
+          <br/>
+          Ajude-nos a povoar o nosso planeta!
+        </p>
+        <button>Entenda o que Aconteceu</button>
+        <button>Ir para o planeta Rick</button>
     </TextContainer>
    </PageContainer>
  );
