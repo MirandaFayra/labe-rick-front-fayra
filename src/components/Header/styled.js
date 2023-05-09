3b5e14c04176d305fe9505af8936b94f0c04f6e1
Junior Prado <juniorcvnnn@gmail.com>
2023-05-09 00:05:12 -0300

    alterrando header

--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 import theme from '../../constants/THEME'
 
 export const HeaderContainer = styled.div`
-    height: 14vh;
     width: 100vw;
     background: ${theme.colors.blue800};
     color:${theme.colors.white};
