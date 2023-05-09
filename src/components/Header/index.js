65c0b1785e7dccee879c85f7d4fad66aaff0ff24
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-05-08 21:32:56 -0300

    Styles: Adicionar seta do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,10 @@
 import * as S from './styled'
+import Arrow  from '../../assets/leftArrow.svg'
 
 export default function Header(){
     return(
         <S.HeaderStyle>
-            <img alt='flexa para esquerda'/>
+            <img src={Arrow} alt='flexa para esquerda'/>
             <p>Labe Rick</p>
             <img alt='icone de casa'/>
         </S.HeaderStyle>
