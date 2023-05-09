08403c56a22b188f9b193f05f837c3871b9f711b
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-05-09 15:51:44 -0300

    Finalizar card

--- src/pages/character-page/index.js
+++ src/pages/character-page/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Card from '../../components/Card';
 
 export default function CharacterPage() {
+
     return (
-        <>
-            <p>Character</p>
+        <>  
+            <p>CharacterPage</p>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
+            <Card/>
         </>
     );
 }
